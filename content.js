da3ba7b09bc58582f278dc0769b993ef2a0319dc
isakcodes <61279302+isakcodes@users.noreply.github.com>
2020-10-16 18:27:33 +0200

    Added styling and buttons to links

--- content.js
+++ content.js
@@ -1,6 +1,62 @@
-// Send a message containing the page details back to the event page
-chrome.runtime.sendMessage({
-    'title': document.title,
-    'url': window.location.href,
-    'summary': window.getSelection().toString()
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    const result = parseGPA();
+    sendResponse(`${result}`);
+
+    function parseGPA () {
+        let betygsMapKTH = new Map([
+            ['A', 5.0],
+            ['B', 4.5],
+            ['C', 4.0],
+            ['D', 3.5],
+            ['E', 3.0],
+        ]);
+        var cards = $("ladok-avslutad-kurs");//.find(".card-body");
+        //console.log(cards.text());
+        var number_of_cards = cards.length;
+        console.log("Found " + number_of_cards + " course cards");
+
+        var arr = [];
+        var points = [];
+        var undef = [];
+        var course_list = [];
+        cards.each(function ( index ) {
+            // TODO: Internationalization *Betyg*
+            var betyg = $( this ).find("strong:contains('Betyg:')").text().split(" ")[1];
+            var kurs  = $( this ).find("ladok-kurslink[kursvy='resultat'] .ldk-visa-desktop").text();
+            //console.log(betyg + "\t" + kurs);
+
+            arr.push(betyg + "<br>" + kurs + "<br>");
+            course_list.push(kurs + "<br>" + betyg + "<br>");
+
+            var thisgrade = betygsMapKTH.get(betyg)
+            if (thisgrade !== undefined) {
+                points.push(thisgrade);
+            } else {
+                undef.push("<b>" + betyg + "</b>" + " i kursen " + kurs);
+            }
+        });
+
+        if (points.length > 0) {
+            var total = points.reduce(function (sum, num) { return sum + num; });
+
+            var gpa = total / points.length;
+            //console.log("Sending message '" + gpa.toFixed(3).toString() + "' ...");
+            //chrome.runtime.sendMessage(gpa.toFixed(3).toString());
+            return gpa;
+            //chrome.runtime.sendMessage("<h2>Ditt betygssnitt: " + gpa + " av " + firstGrade + "</h2>");
+        }
+
+        return points.length; // test
+        
+
+        if (undef.length) {
+            chrome.runtime.sendMessage("<p class='grey'>" + undef.length + " betyg kunde ej värderas och togs ej med i beräkningen:</p>");
+            undef.forEach(value => {
+                chrome.runtime.sendMessage(value + "<br>");
+            })
+        }
+
+        chrome.runtime.sendMessage("<h3>Kurslista</h3>")
+        course_list.forEach(v => { chrome.runtime.sendMessage(v); });
+    }
 });
--- popup.js
+++ popup.js
@@ -1,55 +1,31 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
-    });
-});
-
-// chrome.runtime.onMessage.addListener(function(message) {
-//     console.log("Recieved message: " + message);
-//     $("p").text("hej");
-// });
-
-chrome.runtime.onMessage.addListener(function(msg) {
-    alert("Ditt betygssnitt: " + msg);
-});
-
-
-// // Inject the payload.js script into the current tab after the popout has loaded
-// window.addEventListener('load', function (evt) {
-// 	chrome.runtime.getBackgroundPage().chrome.tabs.executeScript(null, {
-// 		file: 'scraper.js'
-// 	});;
-//     /*$(document).ready(function() {
-//         console.log($("ladok-avslutad-kurs strong:contains('Betyg')")); // .each()
-//     })*/
-// });
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
-
-window.onload = function() {
-    if (window.jQuery) {
-        $(document).ready(function(){
-
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    //refresh();
+    
+    document.getElementById('refresh-button').addEventListener('click', refresh, false);
+    
+    function refresh() {
+        chrome.tabs.query({currentWindow: true, active: true},
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, 'hi', setGrade);
+            });
     }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
+    
+    function setGrade(response) {
+        console.log(response);
+        if (response !== undefined) {
+            document.getElementById("gpa").innerHTML = `${response}`;
+        }
     }
-});
+
+    const login_url = "https://www.student.ladok.se/student/loggain";
+    const grades_url = "https://www.student.ladok.se/student/#/avslutade";
+
+    document.getElementById("login").addEventListener(
+        'click', function() { chrome.tabs.create({active: true, url: login_url}); }, false);
+
+    // TODO: Om rätt ladoksida är uppe, dölj texten som säger åt dig att logga in i ladok
+
+    document.getElementById("grades").addEventListener(
+        'click', function() { chrome.tabs.create({active: true, url: grades_url}); }, false);
+
+}, false);
